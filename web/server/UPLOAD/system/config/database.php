fc7446c48deffe8f7783ba5e19ff69fa4b22019d
allengaller <allengaller@qq.com>
2014-12-19 10:09:29 +0800

    update

---
+++ web/server/UPLOAD/system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => 'QPWOei1029',
+  'dbname' => 'newbee',
+);
+$config['slave'] = false;
